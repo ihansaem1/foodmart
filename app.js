e9efc5f7d874af7b9945b1e157e3853bee5bfb32
ihansaem1 <ihanseam39@gmail.com>
2024-05-03 11:33:08 +0900

    start

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// 라우팅
+app.get('/', (req, res) => {
+    res.render("index"); // ./views/index.ejs
+});
+
+app.listen(port, () => {
+    console.log(`서버가 실행되었습니다. 접속주소 : http://localhost:${port}`);
+});
